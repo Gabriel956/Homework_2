35801bc442656198b16200cd64252cd17d113510
Gabriel956 <gobaez1@gmail.com>
2024-11-13 16:19:12 -0600

    Add project files.

---
+++ js/example.js
@@ -0,0 +1,37 @@
+var listItems = document.getElementsByTagName('li');                   // All <li> elements
+
+// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+var heading = document.querySelector('h2');                        // h2 element
+var headingText = heading.firstChild.nodeValue;                    // h2 text
+var totalItems = listItems.length;                                 // No. of <li> elements
+var newHeading =  headingText + '<span>' + totalItems + '</span>'; // Content
+
+
+/*  WRITE YOUR CODE BELOW */
+
+
+// Part1. Hint: Use the listItems object to iterate through the elements of the list
+for(var i = 0; i < listItems.length; ++i){
+    if(listItems[i].innerHTML == "fresh figs" || listItems[i].innerHTML == "kale" || listItems[i].innerHTML == "honey"){
+
+        listItems[i].className = 'cool';
+    }
+  
+}
+//listItems = 'cool';
+//listItems.className = 'cool';
+//listItems[1].className = 'cool';
+  //if(listItems[i].innerHTML == 'kale'){
+    //listItems[i].className = 'cool';
+   // }
+        //listItems.element.className = 'cool';
+        //listItems[i].classList.add("cool");
+
+
+
+// Part2. Hint: Use the heading and newHeading to show the number of items in the shopping list
+heading.innerHTML = newHeading;
+
+
+
+
